80e8efc880917770969723957831a12432fcdf43
Antony <107538960+JohnyPanamaman@users.noreply.github.com>
2022-06-15 15:21:38 +0300

    Add files via upload

--- CarDealer/src/main/java/ConControl/ConControl.java
+++ CarDealer/src/main/java/ConControl/ConControl.java
@@ -1,23 +1,60 @@
 package ConControl;
+import jdk.jshell.spi.ExecutionControl;
+
 import java.sql.*;
 
 public class ConControl {
-    public void DataReq(){
-        try(Connection conection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cdbase", "root", "12345")){
-            Statement st = conection.createStatement();
+    public void getClient(int id) throws SQLException {
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cdbase", "root", "12345")){
+            Statement st = connection.createStatement();
             String query = "SELECT * FROM client";
             ResultSet resultSet= st.executeQuery(query);
-            System.out.println("Client ID\tName\tSurname\tPhone number\tE-Mail");
+            System.out.println("Client ID\tName\tSurname\tPhone\tE-Mail");
             while(resultSet.next()){
-                System.out.println(resultSet.getInt("Client ID")+
-                        "\t"+resultSet.getString("Name")+
-                        "\t"+resultSet.getString("Surname")+
-                        "\t"+resultSet.getString("Phone numberl")+
-                        "\t"+resultSet.getString("E-Mail"));
+                if (resultSet.getInt("ClientID") == id)
+                    System.out.println(resultSet.getInt("ClientID")+
+                            "\t"+resultSet.getString("Name")+
+                            "\t"+resultSet.getString("Surname")+
+                            "\t"+resultSet.getString("Phone")+
+                            "\t"+resultSet.getString("EMail"));
             }
             st.close();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
+
+    public void addClient(String ClientName, String ClientSurname, String ClientPhoneNum, String ClientEMail) throws SQLException {
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cdbase", "root", "12345")){
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "INSERT INTO client(Name, Surname, Phone, EMail) VALUES ('"
+                    + ClientName + "', '"
+                    + ClientSurname+"', '"
+                    + ClientPhoneNum+ "', '"
+                    +ClientEMail +"');");
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+    public void uppdateClient(int id, String ClientPhoneNum) throws SQLException {
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cdbase", "root", "12345")){
+            Statement statement = connection.createStatement();
+            String updSql = "update client set Phone ='" + ClientPhoneNum + "' WHERE ClientID = '" + id + "';";
+            statement.executeUpdate(updSql);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void deleteClient(String ClientSurname) throws SQLException {
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cdbase", "root", "12345")){
+            Statement statement = connection.createStatement();
+            String updSql = "delete from client WHERE Surname = '" + ClientSurname + "';";
+            statement.executeUpdate(updSql);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
+
--- CarDealer/src/main/java/org/example/Main.java
+++ CarDealer/src/main/java/org/example/Main.java
@@ -2,10 +2,15 @@ package org.example;
 
 import ConControl.ConControl;
 
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("I'am a GAY!");
         ConControl CC=new ConControl();
-        CC.DataReq();                                            //Getting request to DB
+        CC.getClient(1);                                        //Getting request to DB
+        CC.addClient("Antony","Nuprey","298047042","logic@bk.ru");  //add a new client
+        CC.uppdateClient(1,"294235264");          //uppdating client info(PhoneNumber)
+        CC.deleteClient("Nuprey");                     //Removing client using Surname field
     }
 }
